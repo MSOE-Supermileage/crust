f651d8c289abea2e58944b9cd372b7c9b8c54155
Matthew Mahnke <mahnkem@msoe.edu>
2015-10-16 11:15:56 -0500

    Add docstrings for get_private_ip() and get_public_ip()

--- crust.py
+++ crust.py
@@ -8,6 +8,11 @@ import syslog
 
 
 def get_private_ip():
+    """Return the private IP address of this Pi.
+
+    The method for obtaining the private IP addresses, using `hostname` returns
+    a space-separated list of private IP addresses for each network interface.
+    """
     private_ip = '0.0.0.0'
 
     platforms = platform.system()
@@ -23,6 +28,12 @@ def get_private_ip():
 
 
 def get_public_ip():
+    """Return the public IP address of this device.
+
+    Since this Pi is assumed to be connected to the Internet, we request our
+    IP address from http://ipinfo.io/. This is much simplier than using system
+    commands.
+    """
     public_ip = '0.0.0.0'
 
     r = requests.get('http://ipinfo.io/json')
